eaac6c09214aee98ac5be0a7119db3e1a4ae7002
mayamouse <cooperpontea@gmail.com>
2023-04-27 22:31:47 -0400

    Changes to js and additional comments

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
 //*if input is philadelphia then do philadelphia call*//
-$.ajax({
-    url: philadelphia,
-    method: 'GET',
-  }).then(function (response) {
-    console.log('Ajax Reponse \n-------------');
-    console.log(response);
-  });
-  
+var philadelphia = 'api.openweathermap.org/data/2.5/forecast?q=Philadelphia&appid=4d93daea6566fa195be2bba47492c101'
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    console.log('XMLHttpRequest Response \n-------------');
+    console.log(xhr.response);
+  }
+};
+xhr.open('GET', philadelphia);
+xhr.send();
