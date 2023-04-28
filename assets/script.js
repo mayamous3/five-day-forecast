97b1d8adacaa14bfff86a124aa2fd5f521dfe128
mayamouse <cooperpontea@gmail.com>
2023-04-27 22:45:27 -0400

    changed to fetch and added https

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,9 @@
-//*if input is philadelphia then do philadelphia call*//
-var philadelphia = 'api.openweathermap.org/data/2.5/forecast?q=Philadelphia&appid=4d93daea6566fa195be2bba47492c101'
 
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === XMLHttpRequest.DONE) {
-    console.log('XMLHttpRequest Response \n-------------');
-    console.log(xhr.response);
-  }
-};
-xhr.open('GET', philadelphia);
-xhr.send();
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=Philadelphia&appid=4d93daea6566fa195be2bba47492c101')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
